f16a73acc82429bd7688ffb463862b7949c37327
Jochen Wieke <wieke@iplon.de>
2016-02-09 15:49:06 +0530

    Update webhndlrs.go

--- cmd/L2/webhndlrs.go
+++ cmd/L2/webhndlrs.go
@@ -8,5 +8,5 @@ import (
 // extend this with the steps shown at 	https://golang.org/doc/articles/wiki/
 
 func handler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+    fmt.Fprintf(w, "Hi there, I will %s!", r.URL.Path[1:])
 }
